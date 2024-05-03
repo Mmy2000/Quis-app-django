458b5a83bbae8e169d5a2b005e4ccdd47e210e4e
Mmy2000 <yahianada005@gmail.com>
2024-05-04 00:58:25 +0300

    starting result app

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'question',
     'quizes',
+    'result',
 ]
 
 MIDDLEWARE = [
---
+++ result/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from quizes.models import Quizes
+# Create your models here.
+class Result(models.Model):
+    user = models.ForeignKey(User,  on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quizes,  on_delete=models.CASCADE)
+    score = models.FloatField()
+
+
+    def __str__(self):
+        return self.pk
+    

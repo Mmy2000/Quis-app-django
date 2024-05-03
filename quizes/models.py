0de6a168ae9176f3f10682f9a2cf03e08f55dee5
Mmy2000 <yahianada005@gmail.com>
2024-05-04 00:39:39 +0300

    starting quiz app

---
+++ quizes/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+DIFF_CSOICES = (
+        ('easy','easy'),
+        ('meduim','meduim'),
+        ('hard','hard'),
+    )
+
+class Quizes(models.Model):
+    name = models.CharField( max_length=150)
+    topic = models.CharField( max_length=150)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="duration of the quiz in minutes")
+    required_scored_to_pass = models.IntegerField(help_text="required score to pass")
+    diffculity = models.CharField( max_length=6 , choices=DIFF_CSOICES)
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+    
+    

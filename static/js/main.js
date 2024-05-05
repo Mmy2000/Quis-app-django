438d5b19e5acc48d87e4d3159c356d9bda909561
Mmy2000 <yahianada005@gmail.com>
2024-05-05 05:50:29 +0300

    list quizes part 6

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const modalBtn = [...document.getElementsByClassName('modal-button')]
+const modalBody = document.getElementById('modal-body-confirm')
+
+modalBtn.forEach(modalBtn => modalBtn.addEventListener("click" , ()=>{
+    const pk = modalBtn.getAttribute('data-pk')
+    const name = modalBtn.getAttribute('data-quiz')
+    const question = modalBtn.getAttribute('data-questions')
+    const difficulty = modalBtn.getAttribute('data-difficulty')
+    const time = modalBtn.getAttribute('data-time')
+    const score = modalBtn.getAttribute('data-pass')
+    console.log(name);
+    modalBody.innerHTML = `<div class="mb-3">Are you sure you want to begin "<b>${name}</b>"?</div>
+    <div class="text-muted">
+    <ul>
+    <li>Difficulty: <b>${difficulty}</b></li>
+    <li>number of question: <b>${question}</b></li>
+    <li>score to pass: <b>${score}%</b></li>
+    <li>Time: <b>${time} min</b></li>
+    </ul>
+    </div>`
+}) 
+    
+);

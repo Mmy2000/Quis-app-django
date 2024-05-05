243c6c24cf678e2f698180c453ee03bb2e0a2a38
Mmy2000 <yahianada005@gmail.com>
2024-05-06 02:02:59 +0300

    list quizes part 8

--- quizes/urls.py
+++ quizes/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "quizes"
 urlpatterns = [
     path('' , views.QuizList.as_view() , name="QuizList"),
-    path('<pk>/' , views.quiz_detail , name="quiz_detail")
+    path('<pk>/' , views.quiz_detail , name="quiz_detail"),
+    path('<pk>/data/' , views.quiz_data_view , name="quiz_data_view"),
 ]
--- quizes/views.py
+++ quizes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Quizes
 from django.views.generic import ListView
+from django.http import JsonResponse
 # Create your views here.
 
 class QuizList(ListView):
@@ -10,3 +11,16 @@ class QuizList(ListView):
 def quiz_detail(request , pk):
     quiz = Quizes.objects.get(pk=pk)
     return render(request , 'quizes/quiz_detail.html' , {'obj':quiz})
+
+def quiz_data_view(request,pk):
+    quiz = Quizes.objects.get(pk=pk)
+    questions = []
+    for q in quiz.get_questions():
+        answers = []
+        for a in q.get_answer():
+            answers.append(a.text)
+        questions.append({str(q):answers})
+    return JsonResponse({
+        'data':questions ,
+        'time': quiz.time
+    })

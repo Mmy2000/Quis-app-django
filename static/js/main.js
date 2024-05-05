c2b5fcef19d6f947a02a1f5121b039d5c318098e
Mmy2000 <yahianada005@gmail.com>
2024-05-06 02:35:32 +0300

    list quizes part 11

--- static/js/main.js
+++ static/js/main.js
@@ -39,8 +39,20 @@ $.ajax({
         data = response.data
         data.forEach(el =>{
             for (const [question , answers] of Object.entries(el)) {
-                console.log(question);
-                console.log(answers);
+                quizBox.innerHTML += `
+                    <hr>
+                    <div class="mb-3">
+                        <b>${question}</b>
+                    </div>
+                `
+                answers.forEach(answer => {
+                    quizBox.innerHTML += `
+                        <div>
+                            <input type="radio" class="ans" id="${question}-${answer}" name="${question}" value="${answer}">
+                            <label for="${question}">${answer}</label>
+                        </div>
+                    `
+                });
             }
         })
     },

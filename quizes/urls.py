6127edc8935999c34048f16ade5f3277fa9f04a6
Mmy2000 <yahianada005@gmail.com>
2024-05-07 00:09:16 +0300

    list quizes part 13

--- quizes/urls.py
+++ quizes/urls.py
@@ -5,5 +5,6 @@ app_name = "quizes"
 urlpatterns = [
     path('' , views.QuizList.as_view() , name="QuizList"),
     path('<pk>/' , views.quiz_detail , name="quiz_detail"),
+    path('<pk>/save/' , views.save_quiz_view , name="save_quiz"),
     path('<pk>/data/' , views.quiz_data_view , name="quiz_data_view"),
 ]
--- quizes/views.py
+++ quizes/views.py
@@ -24,3 +24,7 @@ def quiz_data_view(request,pk):
         'data':questions ,
         'time': quiz.time
     })
+
+def save_quiz_view(request , pk):
+    print(request.POST)
+    return JsonResponse({'text':'works'})

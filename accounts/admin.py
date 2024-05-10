71958b7a0cdd4c3fc0dec57a8a00d3405c9936c6
Mmy2000 <yahianada005@gmail.com>
2024-05-11 01:35:30 +0300

    creating accounts app part 4

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,9 +4,9 @@ from django.utils.html import format_html
 
 class ProfileAdmin(admin.ModelAdmin):
 
-    def thumbnail(self,object):
-        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.image.url))
-    thumbnail.short_description = "Profile Picture"
+    # def thumbnail(self,object):
+    #     return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.image.url))
+    # thumbnail.short_description = "Profile Picture"
 
     list_display = ('user','full_name','full_address')
 

d0077e9e0474231107f2f32583f92831e361921a
Mmy2000 <yahianada005@gmail.com>
2024-05-11 13:45:53 +0300

    finishing the project

--- accounts/models.py
+++ accounts/models.py
@@ -15,7 +15,11 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='users/')
     about = models.TextField(max_length=500 , blank=True , null=True)
     country = models.CharField(max_length=50 ,blank=True, null=True)
-    created_at = models.DateTimeField( default=timezone.now())
+    created_at = models.DateTimeField( default=timezone.now)
+    fb_link = models.URLField( max_length=200 , blank=True , null=True)
+    twitter_link = models.URLField( max_length=200, blank=True , null=True)
+    instagram_link = models.URLField( max_length=200, blank=True , null=True)
+    linked_in_link = models.URLField( max_length=200, blank=True , null=True)
 
 
     def full_name(self):

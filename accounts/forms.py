ba20d5ae5f56dcc6baa62b13a2c0b821f1472cb5
Mmy2000 <yahianada005@gmail.com>
2024-05-11 13:49:42 +0300

    finishing the project

--- accounts/forms.py
+++ accounts/forms.py
@@ -19,4 +19,4 @@ class UserForm(forms.ModelForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['phone_number','image','country','city','about']
+        fields = ['phone_number','image','country','city','about','fb_link','twitter_link','instagram_link','linked_in_link']
